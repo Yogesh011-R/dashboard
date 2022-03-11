421bf662f90c1857b06c6e412372585554a05fb9
Yogesh Kumar <yogesh.kumar.rr5@gmail.com>
2022-03-11 14:28:26 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Home from './pages/Home.svelte';
+  import { Router, Link, Route } from 'svelte-routing';
+
+  import List from './pages/List.svelte';
+  import Single from './pages/Single.svelte';
+  import New from './pages/New.svelte';
+</script>
+
+<main>
+  <Router>
+    <Route path="/"><Home /></Route>
+    <!-- Users -->
+    <Route path="/users"><List /></Route>
+    <Route path="/users/:userId"><Single /></Route>
+    <Route path="/users/new"><New /></Route>
+
+    <!-- Products -->
+    <Route path="/products"><List /></Route>
+    <Route path="/products/:productId"><Single /></Route>
+    <Route path="/products/new"><New /></Route>
+  </Router>
+</main>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,9 @@
+<div><p>test</p></div>
+
+<style lang="scss">
+  div {
+    p {
+      color: red;
+    }
+  }
+</style>
